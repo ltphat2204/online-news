a376474376bbdee7feee26d7e9121cb76656459a
@voduytan23 <vdtan22@clc.fitus.edu.vn>
2024-12-07 00:06:25 +0700

    commit before pull

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,55 +1,63 @@
 import { getUserByEmail,
-         getUserByUsername,
-         createUser
- } from "../models/user.js";
+    getUserByUsername,
+    createUser
+} from "../models/user.js";
 import { comparePassword,
-         hashPassword} from "../utils/cryptography.js";
+    hashPassword} from "../utils/cryptography.js";
 
 export const handleLogin = async (req, res) => {
-    const user = req.body;
-    const old_user = await getUserByEmail(user.email) || await getUserByUsername(user.email);
-    if (!old_user) {
-        res.render("auth/login", {
-            title: "Đăng nhập",
-            userNotFound: true
-        });
-    }
-    else {
-        const validity = await comparePassword(user.password, old_user.password);
-        if (!validity) {
-            res.render("auth/login", {
-                title: "Đăng nhập",
-                wrongPassword: true
-            });
-        }
-        else {
-            res.redirect("/");
-        }
-    }
+const user = req.body;
+const old_user = await getUserByEmail(user.email) || await getUserByUsername(user.email);
+if (!old_user) {
+   res.render("auth/login", {
+       title: "Đăng nhập",
+       userNotFound: true
+   });
+}
+else {
+   const validity = await comparePassword(user.password, old_user.password);
+   if (!validity) {
+       res.render("auth/login", {
+           title: "Đăng nhập",
+           wrongPassword: true
+       });
+   }
+   else {
+       req.session.auth = true;
+       req.session.authUser = old_user;
+       res.redirect("/");
+   }
+}
 }
 
+export const handleLogout = async(req, res) => {
+req.session.auth = false;
+req.session.authUser = null;
+req.session.retUrl = null;
+res.redirect("/");
+}
 export const handleRegister = async (req, res) => {
-    const user = req.body;
-    user.role = "subscriber";
-    const old_user = await getUserByEmail(user.email);
-    const old_name = await getUserByUsername(user.username);
+const user = req.body;
+user.role = "subscriber";
+const old_user = await getUserByEmail(user.email);
+const old_name = await getUserByUsername(user.username);
 
-    if (old_user) {
-        res.render("auth/register", {
-            title: "Đăng ký",
-            userExist: true
-        });
-    }
-    else if (old_name) {
-        res.render("auth/register", {
-            title: "Đăng ký",
-            userExist: true
-        });
-    }
-    else {
-        const hashedPassword = await hashPassword(user.password);
-        user.password = hashedPassword;
-        await createUser(user);
-        res.redirect("/");
-    }
+if (old_user) {
+   res.render("auth/register", {
+       title: "Đăng ký",
+       userExist: true
+   });
+}
+else if (old_name) {
+   res.render("auth/register", {
+       title: "Đăng ký",
+       userExist: true
+   });
+}
+else {
+   const hashedPassword = await hashPassword(user.password);
+   user.password = hashedPassword;
+   await createUser(user);
+   res.redirect("/");
+}
 }
