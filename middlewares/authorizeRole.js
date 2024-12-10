d52cb716de1cc676547fcbd36bb2168aa15bf778
@voduytan23 <vdtan22@clc.fitus.edu.vn>
2024-12-10 13:20:04 +0700

    fix authorizeRole when not login yet

--- middlewares/authorizeRole.js
+++ middlewares/authorizeRole.js
@@ -1,6 +1,10 @@
 import { authUserById } from "../controllers/user.js";
 const authorizeRole = (allowedRoles) =>{
     return async function(req, res, next) {
+        if(req.session.authUser === undefined){
+            res.redirect("/auth/login");
+            return;
+        }
         const id = req.session.authUser.id;
         const user = await authUserById(id);
         if (allowedRoles.includes(user.role)) {
