92024cfd0089c91ce76bc412142f9c1e4303a245
ltphat2204 <ltphat2204@gmail.com>
2025-01-01 21:58:00 +0700

    Fix url for profile page

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,10 +1,11 @@
 import { getArticlesByWriterUsername } from "../models/articles.js";
-import { getWriterByUsername } from "../models/user.js";
+import { getUserByUsername } from "../models/user.js";
 
 export const viewProfile = async (req, res) => {
     const { username } = req.params;
+    console.log(req.session)
 
-    const profile = await getWriterByUsername(username);
+    const profile = await getUserByUsername(username);
     const articles = await getArticlesByWriterUsername(username);
 
     res.render('profile/profile', {
