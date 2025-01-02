d3472d0315d1432771809282eaad7046299196c2
Lê Trường <truongle.25082004@gmail.com>
2025-01-02 13:56:21 +0700

    update main and admin layouts

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  if (!req.session.auth) {
-    req.session.auth = false;
-  }
   res.render('home', {
     title: 'Trang chủ',
   });
